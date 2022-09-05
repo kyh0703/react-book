6bd51f331f06adb0a95aef2daba53d034eb42c40
Kimyeonho <kyh0703@nate.com>
2022-09-05 12:19:14 +0900

    [Feature] webpack

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import CSSModule from "./CSSModule";
 import SassComponent from "./SassComponent";
 
 function App() {
   return (
     <div className="App">
       <SassComponent />
+      <CSSModule />
     </div>
   );
 }
