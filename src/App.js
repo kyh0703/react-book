cc7063779b564f368e75d4d511083f1a0749e958
Kimyeonho <kyh0703@nate.com>
2022-09-13 10:27:27 +0900

    [Feature]  Styled

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import CSSModule from "./CSSModule";
 import SassComponent from "./SassComponent";
+import StyledComponent from "./StyledComponent";
 
 function App() {
   return (
     <div className="App">
-      <SassComponent />
-      <CSSModule />
+      <StyledComponent />
     </div>
   );
 }
