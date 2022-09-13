a5d3a1fdde9ffa654e293175edc5ed770548067e
Kimyeonho <kyh0703@nate.com>
2022-09-13 16:27:23 +0900

    [Feature] 최적화 - useReducer

--- src/App.js
+++ src/App.js
@@ -4,60 +4,61 @@ import TodoTemplate from "./components/TodoTemplate";
 import TodoList from "./components/TodoList";
 import { useState, useCallback, useRef } from "react";
 import { toIdentifier } from "../../../../AppData/Local/Microsoft/TypeScript/4.8/node_modules/@babel/types/lib/index";
+import { useReducer } from "react";
 
-function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: "리엑트의 기초 알아보기",
-      checked: true,
-    },
-    {
-      id: 2,
-      text: "컴포넌트 스타일링 해보기",
-      checked: true,
-    },
-    {
-      id: 3,
-      text: "일정 관리 앱 만들어 보기",
+const createBulkTodos = () => {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+};
 
-  const nextId = useRef(4);
+const toDoReducer = (todos, action) => {
+  switch (action.type) {
+    // {type: "INSERT", todo: {id: 1, text: 'todo', checked: false}}
+    case "INSERT":
+      return todos.concat(action.todo);
+    // {type: "REMOVE", id: 1}
+    case "REMOVE":
+      return todos.filter((todo) => todo.id !== action.id);
+    // {type: "TOGGLE", id: 1}
+    case "TOGGLE":
+      return todos.map((todo) =>
+        todo.id === action.id ? { ...todo, checked: !todo.checked } : todo
+      );
+    default:
+      return todos;
+  }
+};
+
+const App = () => {
+  const [todos, dispatch] = useReducer(toDoReducer, undefined, createBulkTodos);
+
+  const nextId = useRef(2501);
 
   // Handle Func
-  const onInsert = useCallback(
-    (text) => {
-      console.log("text onInsert", text);
-      const todo = {
-        id: nextId.current,
-        text,
-        checked: false,
-      };
-      setTodos(todos.concat(todo));
-      nextId.current += 1;
-    },
-    [todos]
-  );
+  const onInsert = useCallback((text) => {
+    const todo = {
+      id: nextId.current,
+      text,
+      checked: false,
+    };
+    dispatch({ type: "CREATE" }, todo);
+    nextId.current += 1;
+  }, []);
 
-  const onRemove = useCallback(
-    (id) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-    },
-    [todos]
-  );
+  const onRemove = useCallback((id) => {
+    dispatch({ type: "REMOVE", id });
+  }, []);
 
-  const onToggle = useCallback(
-    (id) => {
-      setTodos(
-        todos.map((todo) =>
-          todo.id === id ? { ...todo, checked: !todo.checked } : todo
-        )
-      );
-    },
-    [todos]
-  );
+  const onToggle = useCallback((id) => {
+    dispatch({ type: "TOGGLE", id });
+  }, []);
 
   return (
     <TodoTemplate>
@@ -65,6 +66,6 @@ function App() {
       <TodoList todos={todos} onRemove={onRemove} onToggle={onToggle} />
     </TodoTemplate>
   );
-}
+};
 
 export default App;
