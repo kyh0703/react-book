50f6ed78f5872acb9b045252f0d038816bff5ce7
Kimyeonho <kyh0703@nate.com>
2022-09-14 22:49:05 +0900

    [Feature] 404 page
    와일드 카드 문자로 404 page 전달

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from 'react-router-dom';
 
-import "./App.css";
-import Layout from "./Layout";
-import About from "./pages/About";
-import Article from "./pages/Article";
-import Articles from "./pages/Articles";
-import Home from "./pages/Home";
-import Profile from "./pages/Profile";
+import './App.css';
+import Layout from './Layout';
+import About from './pages/About';
+import Article from './pages/Article';
+import Articles from './pages/Articles';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route index element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/profiles/:username' element={<Profile />} />
       </Route>
-      <Route path="/articles" element={<Articles />}>
-        <Route path=":id" element={<Article />} />
+      <Route path='/articles' element={<Articles />}>
+        <Route path=':id' element={<Article />} />
       </Route>
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
