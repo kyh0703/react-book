c09bb85ec88c6ba74bc30ecf0770cc9b15d758da
Kimyeonho <kyh0703@nate.com>
2022-09-14 22:30:32 +0900

    [Feature] 중첩된 라우트
    중첩된 라우터
    Outlet은 중첩된 자식 컴포넌트를 렌더링함
    
    * 헤더로 먼저 만들어 둔 후 layout을 정한후에 Outlet을 통해 구조를 잡아도됨

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import { Route, Routes } from "react-router-dom";
 
 import "./App.css";
+import Layout from "./Layout";
 import About from "./pages/About";
+import Article from "./pages/Article";
+import Articles from "./pages/Articles";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
+      <Route path="/articles" element={<Articles />}>
+        <Route path=":id" element={<Article />} />
+      </Route>
     </Routes>
   );
 }
