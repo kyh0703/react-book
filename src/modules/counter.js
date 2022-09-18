bfd5c19d071f095c044469e3802bc24b664a717f
Kimyeonho <kyh0703@nate.com>
2022-09-18 21:16:29 +0900

    [Feature] Redux - defined Redux module

---
+++ src/modules/counter.js
@@ -0,0 +1,31 @@
+// 액션 정의
+// {ModuleName}/{ActionName}
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+// 액션 생성 함수
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+// 초기 상태
+const initialState = {
+  number: 0,
+};
+
+// Reducer
+const counter = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+export default counter;
