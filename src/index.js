a94ef9d48c214fa75651a8a963d11baa67cb51e3
Kimyeonho <kyh0703@nate.com>
2022-09-18 21:52:28 +0900

    [Feature] Redux devtools
    1. Redux Devtool
    2. createStore -> legacy로 인해 toolkit을 권장한다. 해당 문서 참조하여 확인이 필요,
    yarn add redux-devtools-extension

--- src/index.js
+++ src/index.js
@@ -2,10 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { legacy_createStore as createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './modules';
+import { Provider } from 'react-redux';
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools()
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
