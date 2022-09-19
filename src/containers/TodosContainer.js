ab9e69a187a73b0ac075c0b4f8e51314fec0205d
Kimyeonho <kyh0703@nate.com>
2022-09-20 00:24:07 +0900

    [Fature] Redux - useActions
    Actions이 많을 경우 useAction으로...
    connect함수로 컨테이너 컴포넌트를 만들경우 리랜더링될때 props가 바뀌지 않다면 자동으로 리렌더링이 방지됨
    
    하지만 useSelector를 사용하여 리덕스 상태를 조회했을때 자동으로 이루어지지않기에 React.memo를 사용하여야함

--- src/containers/TodosContainer.js
+++ src/containers/TodosContainer.js
@@ -2,21 +2,18 @@ import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeInput, insert, toggle, remove } from '../modules/todos';
 import Todos from '../components/Todos';
+import useAction from '../lib/useActions';
 
 const TodosContainer = () => {
   const { input, todos } = useSelector(({ tdoos }) => ({
     input: todos.input,
     todos: todos.todos,
   }));
-  const dispatch = useDispatch();
-  const onChangeInput = useCallback(
-    (input) => dispatch(changeInput(input)),
-    [dispatch]
-  );
-  const onInsert = useCallback((text) => dispatch(insert(text)), [dispatch]);
-  const onToggle = useCallback((id) => dispatch(toggle(id)), [dispatch]);
-  const onRemove = useCallback((id) => dispatch(insert(id)), [dispatch]);
 
+  const [onChangeInput, onInsert, onToggle, onRemove] = useAction(
+    [changeInput, insert, toggle, remove],
+    []
+  );
   return (
     <Todos
       input={input}
@@ -29,4 +26,4 @@ const TodosContainer = () => {
   );
 };
 
-export default TodosContainer;
+export default React.memo(TodosContainer);
