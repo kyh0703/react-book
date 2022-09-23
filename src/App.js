cf315a1aaa10a2216eca694ff4137977c344a7b4
Kimyeonho <kyh0703@nate.com>
2022-09-23 16:00:03 +0900

    [Feature] 코드스플리팅
    리엑트 프로젝트에서 별도로 설정하지 않으면, A, B, C컴포넌트에 대한 코드가 모두 한 파일에 저장되어 버리게 됨. 그러면서 파일의 크기도 커지고 로딩이 오래 걸리게 되는데 이 점을 해결하기 위해 "코드 비동기 로딩"이라는 개념이 있음.
    
    필요한 시점에 자바스크립트 함수, 객체, 컴포넌트를 불러와서 사용가능

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import notify from './notify';
 
 function App() {
+  const onClick = () => {
+    notify();
+  };
+
   return (
     <div className='App'>
-      <header className='App-header'></header>
+      <header className='App-header'>
+        <p onClick={onClick}>hello react!</p>
+      </header>
     </div>
   );
 }
