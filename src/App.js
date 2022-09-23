1ad25fa9aa6e65fe9edfebc742e55f77192b594e
Kimyeonho <kyh0703@nate.com>
2022-09-23 16:26:10 +0900

    [Feature] 리액트 스필리팅 - loadable Component
    서드파티 라이브러리
    
    서버사이드 렌더링 지원
    
    렌더링하기 전 필요할 때 스플리팅된 파일을 미리 불러 올 수 있음.
    
    서버사이드 렌더링이란 웹서비스의 초기 로딩 속도 개선, 캐싱 및 검색 엔진 최적화를 가능하게 하는 기술
    
    사용자 브라우저가 아닌 서버 쪽에서 초기렌더링 처리

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
 import React, { useState, Suspense } from 'react';
 import './App.css';
+import loadable from '@loadable/component';
 
-const SplitMe = React.lazy(() => import('./SplitMe'));
+const SplitMe = loadable(
+  () => import('./SplitMe'),
+  // 로딩중에 다른 UI를 보여주고 싶다면
+  { fallback: <div>loading...</div> }
+);
 
 function App() {
   const [visible, setVisible] = useState(false);
   const onClick = () => {
     setVisible(true);
   };
+  const onMouseOver = () => {
+    SplitMe.preload();
+  };
 
   return (
     <div className='App'>
       <header className='App-header'>
-        <p onClick={onClick}>hello react!</p>
-        <Suspense fallback={<div>Loaidng...</div>}>
-          {visible && <SplitMe />}
-        </Suspense>
+        <p onClick={onClick} onMouseOver={onMouseOver}>
+          hello react!
+        </p>
+        {visible && <SplitMe />}
       </header>
     </div>
   );
