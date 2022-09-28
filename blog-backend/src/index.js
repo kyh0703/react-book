6eca346d39dab713245515901b88c237670545d4
Kimyeonho <kyh0703@nate.com>
2022-09-28 13:11:09 +0900

    [Feature] koa
    koa
    koa-router
    nodemon

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,28 +1,18 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const api = require('./api');
 
 const app = new Koa();
+const router = new Router();
 
-app.use((ctx, next) => {
-  console.log(ctx.url);
-  console.log(1);
+router.use('/api', api.routes());
 
-  if (ctx.query.authorized !== '1') {
-    ctx.status = 401;
-    return;
-  }
-  next().then(() => {
-    console.log('End');
-  });
-});
+// 라우터 적용 전에 적용
+app.use(bodyParser());
 
-app.use((ctx, next) => {
-  console.log(2);
-  next();
-});
-
-app.use((ctx) => {
-  ctx.body = 'hello world';
-});
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
   console.log('Listening to port 4000');
