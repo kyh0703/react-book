678c77e85bfaeafc7996eae9c3a0252918fb90a2
Kimyeonho <kyh0703@nate.com>
2022-09-29 00:01:50 +0900

    [Feature] JWT 회원인증 진행(1)
    ya bcryt

---
+++ blog-backend/src/api/auth/auth.ctrl.js
@@ -0,0 +1,50 @@
+const Joi = require('joi');
+const User = require('../../models/user');
+
+/*
+    POST /api/auth/register
+    {
+        username: 'velopert',
+        password: 'mypass123'
+    }
+*/
+export const register = async (ctx) => {
+  // Request Body 검증하기
+  const schema = Joi.object().keys({
+    username: Joi.string().alphanum().min(3).max(20).required(),
+    password: Joi.string().required(),
+  });
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { username, password } = ctx.request.body;
+  try {
+    const exist = await User.findByUsername(username);
+    if (exist) {
+      ctx.status = 409; // Conflict
+      return;
+    }
+
+    const user = new User({
+      username,
+    });
+    await user.setPassword(password);
+    await user.save();
+
+    const data = user.toJSON();
+    delete data.hashedPassword;
+    ctx.body = data;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
+
+export const login = async (ctx) => {};
+
+export const check = async (ctx) => {};
+
+export const logout = async (ctx) => {};
---
+++ blog-backend/src/models/user.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+
+const UserSchema = new Schema({
+  username: String,
+  hashedPassword: String,
+});
+
+UserSchema.methods.setPassword = async function (password) {
+  const hash = await bcrypt.hash(password, 10);
+  this.hashedPassword = hash;
+};
+
+UserSchema.method.checkPassword = async function (password) {
+  const result = await bcrypt.compare(password, this.hashedPassword);
+  return result;
+};
+
+UserSchema.statics.findByUsername = function (username) {
+  return this.findOne({ username });
+};
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
