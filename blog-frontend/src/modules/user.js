fb29ac6ac5aa3070e3992c06c63e1aacd75c8c67
Kimyeonho <kyh0703@nate.com>
2022-10-04 15:40:44 +0900

    [Feature] Logout

--- blog-frontend/src/modules/user.js
+++ blog-frontend/src/modules/user.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { takeLatest } from 'redux-saga/effects';
+import { call, takeLatest } from 'redux-saga/effects';
 import * as authAPI from '../lib/api/auth';
 import createRequestSaga, {
   createRequestActionTypes,
@@ -7,12 +7,17 @@ import createRequestSaga, {
 
 // 새로고침 이후 임시 로그인 처리
 const TEMP_SET_USER = 'user/TEMP_SET_USER';
+
 // 회원 정보 확인
 const [CHECK, CHECK_SUCCESS, CHECK_FAILURE] =
   createRequestActionTypes('user/CHECK');
 
+// 로그 아웃
+const LOGOUT = 'user/LOGOUT';
+
 export const tempSetUser = createAction(TEMP_SET_USER, (user) => user);
 export const check = createAction(CHECK);
+export const logout = createAction(LOGOUT);
 
 const checkSaga = createRequestSaga(CHECK, authAPI.check);
 
@@ -24,9 +29,19 @@ function checkFailureSaga() {
   }
 }
 
+function* logoutSaga() {
+  try {
+    yield call(authAPI.logout);
+    localStorage.removeItem('user');
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 export function* userSaga() {
   yield takeLatest(CHECK, checkSaga);
   yield takeLatest(CHECK_FAILURE, checkFailureSaga);
+  yield takeLatest(LOGOUT, logoutSaga);
 }
 
 const initialState = {
@@ -49,6 +64,10 @@ export default handleActions(
       ...state,
       user: null,
       checkError: error,
+    }),
+    [LOGOUT]: (state) => ({
+      ...state,
+      user: null,
     }),
   },
   initialState,
