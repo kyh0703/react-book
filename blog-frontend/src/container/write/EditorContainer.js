bd7a7acadcb12abf872e20a7f8e9f3bbd5b53e44
Kimyeonho <kyh0703@nate.com>
2022-10-05 13:15:02 +0900

    [Feature] Post 등록

--- blog-frontend/src/container/write/EditorContainer.js
+++ blog-frontend/src/container/write/EditorContainer.js
@@ -1,4 +1,4 @@
-import { useEffect, useCallback } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import Editor from '../../components/write/Editor';
 import { useSelector, useDispatch } from 'react-redux';
 import { changeField, initialize } from '../../modules/write';
@@ -9,19 +9,16 @@ const EditorContainer = () => {
     title: write.title,
     body: write.body,
   }));
-
   const onChangeField = useCallback(
     (payload) => dispatch(changeField(payload)),
     [dispatch],
   );
-
-  // 언마운트될 때 초기화
   useEffect(() => {
+    // 언마운트 될 때 초기화
     return () => {
       dispatch(initialize());
     };
   }, [dispatch]);
-
   return <Editor onChangeField={onChangeField} title={title} body={body} />;
 };
 
--- blog-frontend/src/pages/WritePage.js
+++ blog-frontend/src/pages/WritePage.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
-import TagBox from '../components/write/TagBox';
-import WriteActionButtons from '../components/write/WriteActionButtons';
-import EditorContainer from '../components/write/EditorContainer';
+import EditorContainer from '../container/write/EditorContainer';
+import TagBoxContainer from '../container/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../container/write/WriteActionButtonsContainer';
 
 const WritePage = () => {
   return (
     <Responsive>
       <EditorContainer />
-      <TagBox />
-      <WriteActionButtons />
+      <TagBoxContainer />
+      <WriteActionButtonsContainer />
     </Responsive>
   );
 };
