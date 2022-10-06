d1677aa238c5f7a0e9c7f3e1b0e6ae1d99dfe45c
Kimyeonho <kyh0703@nate.com>
2022-10-06 13:07:52 +0900

    [Feature] Post Delete

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -4,19 +4,25 @@ import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import WritePage from './pages/WritePage';
 import PostPage from './pages/PostPage';
+import { Helmet } from 'react-helmet-async';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<PostListPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/write" element={<WritePage />} />
-      <Route path="/@:username">
-        <Route index element={<PostListPage />} />
-        <Route path=":postId" element={<PostPage />} />
-      </Route>
-    </Routes>
+    <>
+      <Helmet>
+        <title>REACTORS</title>
+      </Helmet>
+      <Routes>
+        <Route path="/" element={<PostListPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/write" element={<WritePage />} />
+        <Route path="/@:username">
+          <Route index element={<PostListPage />} />
+          <Route path=":postId" element={<PostPage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
